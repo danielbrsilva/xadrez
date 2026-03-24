93e6d187a3e318713a4336b167572c9088f551ad
danielbrsilva <danielbernadorod@gmail.com>
2026-03-24 13:21:09 -0300

    colocando as casas no tabuleiro, os ids nas casas e centralizando o tabuleiro

---
+++ src/script.js
@@ -0,0 +1,20 @@
+const tabuleiro = document.getElementById("tabuleiro");
+
+function criarTabuleiro() {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            const casa = document.createElement("div"); // Criando um elemento div para cada casa do tabuleiro
+            casa.classList.add("casa"); // Adicionando a classe "casa" para estilização
+            const letra = String.fromCharCode(64 + i + 1); // Convertendo número para letra (A-H)
+            casa.id = `${letra}${j + 1}`; // Colocando o ID na casa (Exemplo: A1, B2, etc.)
+            if ((i + j) % 2 === 0) {
+                casa.classList.add("casa_clara"); // Adicionando a classe "casa_clara" para casas claras
+            } else {
+                casa.classList.add("casa_escura"); // Adicionando a classe "casa_escura" para casas escuras
+            }
+            tabuleiro.appendChild(casa); // Adicionando a casa ao tabuleiro
+        }
+    }
+}
+
+criarTabuleiro();
